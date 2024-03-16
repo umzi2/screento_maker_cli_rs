e44dfd0c2d120bc6145845f00578f76a927a086f
umzi <ab5447612@gmail.com>
2024-03-16 18:12:44 +0300

    code: clear

--- src/utils/screenton.rs
+++ src/utils/screenton.rs
@@ -23,27 +23,20 @@ impl Screenton {
         }
     }
     pub fn run(&mut self, array: &mut Array2<f32>) {
-        let dot_size = self.dot_size;
-        let dot = &self.dot;
-        let dot_inv = &self.dot_inv;
-        let ly_plus = &self.ly_plus;
-        let lx_plus = &self.lx_plus;
-        let mut src_values:f32;
-        let mut colum :usize;
-        let(w,h)=(array.shape()[0],array.shape()[1]);
-        let ww=0..w;
-        let hh=0..h;
-        for ly in ww {
-            let ly2 = ly+ly_plus;
-            colum = ly2/self.dot_size;
-            for lx in hh.clone() {
+        let(h,w)=(array.shape()[0],array.shape()[1]);
+
+        for ly in 0..h {
+            let ly2 = ly+self.ly_plus;
+            let colum = ly2/self.dot_size;
+
+            for lx in 0..w {
                 let value = &mut array[[ly, lx]];
                 if *value > 0.0 && *value < 1.0 {
-                    let lx2 = lx+lx_plus;
-                    src_values= if (colum+ lx2/dot_size) % 2 == 1 {
-                        dot_inv[[lx2 % dot_size,ly2 % dot_size]]
+                    let lx2 = lx+self.lx_plus;
+                    let src_values= if (colum+ lx2/self.dot_size) % 2 == 1 {
+                        self.dot_inv[[lx2 % self.dot_size,ly2 % self.dot_size]]
                     } else {
-                        dot[[lx2 % dot_size,ly2 % dot_size]]
+                        self.dot[[lx2 % self.dot_size,ly2 % self.dot_size]]
                     };
                     let src_value = src_values;
                     *value = if *value < src_value { 0.0 } else { 1.0 };
